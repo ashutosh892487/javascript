f68eba9400f1c693c127b5161e32971dbcf77b72
ashutosh892487 <ashutosh892487@gmail.com>
2018-10-16 17:38:35 +0530

    updated

--- ex13.js
+++ ex13.js
@@ -1,18 +1,6 @@
-var flatten = function(a, shallow,r){
-  if(!r){ r = []}
-   
-if (shallow) {
-  return r.concat.apply(r,a);
-  }
-      
-   for(var i=0; i<a.length; i++){
-        if(a[i].constructor == Array){
-            flatten(a[i],shallow,r);
-        }else{
-            r.push(a[i]);
-        }
-    }
-    return r;
+var myObj = [5, [22], [[14]], [[4]],[5,6]];
+function flattenDeep(myObj) {
+  return myObj.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
 }
-
-console.log(flatten([5, [22], [[14]], [[4]],[5,6]]));
+var arr=flattenDeep(myObj);
+console.log(arr);
